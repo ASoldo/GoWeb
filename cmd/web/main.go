3491824283fe5b2047eabe6c82348b77b36372ab
ASoldo <soldo.andrej@gmail.com>
2022-09-20 01:49:52 +0200

    Implement Sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,16 +4,46 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/ASoldo/go-web/pkg/config"
 	"github.com/ASoldo/go-web/pkg/handlers"
 	"github.com/ASoldo/go-web/pkg/render"
+	"github.com/alexedwards/scs/v2"
 
 	"runtime/trace"
 )
 
+var app config.AppConfig
+var session *scs.SessionManager
+
+// Clean returns the shortest path name equivalent to path
+// by purely lexical processing. It applies the following rules
+// iteratively until no further processing can be done:
+//
+//  1. Replace multiple slashes with a single slash.
+//  2. Eliminate each . path name element (the current directory).
+//  3. Eliminate each inner .. path name element (the parent directory)
+//     along with the non-.. element that precedes it.
+//  4. Eliminate .. elements that begin a rooted path:
+//     that is, replace "/.." by "/" at the beginning of a path.
+//
+// The returned path ends in a slash only if it is the root "/".
+//
+// If the result of this process is an empty string, Clean
+// returns the string ".".
+//
+// See also Rob Pike, “[Lexical File Names in Plan 9].”
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		fmt.Println("Cannot create template cache ", err)
